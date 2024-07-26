362fb2bc5422abf5f4f4768d3d0c12d83352f8ac
bae080311 <s24024@gsm.hs.kr>
2024-07-27 02:25:02 +0900

    feat: authentication

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCRSHs0eDPUtyC4TPSvha4a7xKuj7AZCYc",
@@ -11,3 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
