6aea92d05dc9ca475b2081c276cac7a3b58194d1
bae080311 <s24024@gsm.hs.kr>
2024-07-27 02:15:08 +0900

    feat: firebase

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRSHs0eDPUtyC4TPSvha4a7xKuj7AZCYc",
+  authDomain: "twitter-68f23.firebaseapp.com",
+  projectId: "twitter-68f23",
+  storageBucket: "twitter-68f23.appspot.com",
+  messagingSenderId: "454943497142",
+  appId: "1:454943497142:web:83db6f9b72143b93003f17",
+  measurementId: "G-RYR4HP64HS",
+};
+
+const app = initializeApp(firebaseConfig);
