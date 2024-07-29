7d737083898289fd5d3af6ab87632655a3d8b6ba
bae080311 <s24024@gsm.hs.kr>
2024-07-29 17:40:59 +0900

    feat: github login

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,29 @@ import Home from "./routes/home";
 import Profile from "./routes/Profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, styled } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
-
+import { auth } from "./firebase";
+import ProtectedRoutes from "./components/Protected-routes";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: (
+      <ProtectedRoutes>
+        <Layout />
+      </ProtectedRoutes>
+    ),
     children: [
-      { path: "", element: <Home /> },
-      { path: "profile", element: <Profile /> },
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
     ],
   },
   { path: "/login", element: <Login /> },
@@ -28,10 +39,18 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
   body {
+  display: flex;
+    justify-content: center;
     background-color: black;
     color: white;
     font-family: 'system-ui', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
 `;
 
 function App() {
@@ -39,6 +58,7 @@ function App() {
 
   useEffect(() => {
     const init = async () => {
+      await auth.authStateReady();
       setIsLoading(false);
     };
 
@@ -46,10 +66,10 @@ function App() {
   }, []);
 
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
       {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-    </>
+    </Wrapper>
   );
 }
 
---
+++ src/components/Protected-routes.tsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { auth } from "../firebase";
+
+export default function ProtectedRoutes({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = auth.currentUser;
+  if (user === null) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+}
